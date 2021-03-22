05216ecf6f48e87e24289c666f3e55d62274c8a2
Alex-Bujorianu <lxbujorianu@gmail.com>
2021-03-22 18:00:10 +0100

    Added test results as a multivariate distribution

--- Part 2/Dx-distribution-fitting.R
+++ Part 2/Dx-distribution-fitting.R
@@ -1,5 +1,8 @@
 #Script to determine distribution of Dx1, Dx2 and Dx3 from the dataset
 
+library("fitdistrplus")
+library("SimDesign")
+
 load("trial_dataset.RData")
 
 #I am only going to fit the data on C1 of Tx1, the first cycle
@@ -51,3 +54,44 @@ hist(Responders)
 hist(Nonresponders)
 
 #Those are some wacky histograms. 
+
+Test1_responders_norm <- fitdist(Test1_responders, distr="norm")
+summary(Test1_responders_norm)
+gofstat(Test1_responders_norm)
+plot(Test1_responders_norm)
+
+Test2_responders_norm <- fitdist(Test2_responders, distr="norm")
+plot(Test2_responders_norm) #OK fit
+
+Test3_responders_norm <- fitdist(Test3_responders, distr="norm")
+plot(Test3_responders_norm)
+
+#Indeed a normal distribution is a very good fit for the individual tests
+
+m_vect <- cbind(Test1_responders,
+                Test2_responders,
+                Test3_responders)
+
+m_cov <- cov(m_vect) # create covariance matrix
+v_means <-  c(mean(Test1_responders), mean(Test2_responders), mean(Test3_responders))
+Test_results_responders <- rmvnorm(n = 1, mean = v_means, sigma = m_cov) #Test results for responders
+
+plot(density(Test1_responders), col = 'red', xlim = c(-5, 10), ylim = c(0, 2), main = 'Density functions') # plot the different density functions
+lines(density(Test2_responders), col = 'darkgray')
+lines(density(Test3_responders), col = 'green')
+legend("topright",                    # Add legend to plot
+       legend = c("Test1_responders", "Test2_responders", "Test3_responders"),
+       col = c('red', 'darkgray', 'green'),
+       lty = 1)
+
+non_m_vect <- cbind(Test1_nonresponders,
+                    Test2_nonresponders,
+                    Test3_nonresponders)
+non_m_cov <- cov(non_m_vect)
+non_v_means <-  c(mean(Test1_nonresponders), mean(Test2_nonresponders), mean(Test3_nonresponders))
+
+Test_results_nonresponders <- rmvnorm(n = 1, mean = non_v_means, sigma = non_m_cov)
+
+#We are doing a multivariate distribution because the tests are correlated
+#MGF theory tells us that if you add 3 normal distributions, you end up with a normal distribution 
+#whose mean is the sum of the other 3 means
