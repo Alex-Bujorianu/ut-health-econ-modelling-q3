d667b91cdaee0d334614b8d469fea1093c1c06c2
Alex-Bujorianu <lxbujorianu@gmail.com>
2021-03-29 14:33:54 +0200

    Did data analysis for PSA. Made all the figures

--- Part_2/PSA-Analysis.R
+++ Part_2/PSA-Analysis.R
@@ -24,9 +24,12 @@ c.bsc <- psa.results$c.bsc
 effects_vector <- vector()
 costs_vector <- vector()
 
+ICERs_vector <- vector()
+
 for (i in 1:length(psa.results$c.bsc)) {
   dif_effects <- e.exp[i] - e.bsc[i]
   dif_costs <- c.exp[i] - c.bsc[i]
+  ICERs_vector <- c(ICERs_vector, (dif_costs / dif_effects))
   effects_vector <- c(effects_vector, dif_effects)
   costs_vector <- c(costs_vector, dif_costs)
 }
@@ -35,8 +38,33 @@ plane <- data.frame(effects_vector, costs_vector)
 plot(plane, xlab="Incremental effects", ylab="Incremental costs")
 
 ##ggplot
-ggplot(plane, aes("Inc costs", "Inc effects")) +
-        xlim(-0.1, 0.15) +
-        ylim(-4000, 4000) +
-         geom_point(data=plane) +
-        geom_abline(intercept = 0, slope = 20000)
+WTP_plot <- ggplot(plane, aes(x=effects_vector, y=costs_vector)) +
+  scale_x_continuous(limits = c(-0.1, 0.15)) + scale_y_continuous(limits = c(-4000, 4000)) +
+  geom_point(data=plane) +
+  geom_abline(intercept = 0, slope = 20000) +
+  geom_abline(intercept = 0, slope = 80000) +
+  xlab("Incremental effects") +
+  ylab("Incremental costs") # for the y axis label
+
+WTP_plot
+        
+##We have to determine how many fall below the threshold. We do this by calculating the individual ICERs.
+length <- length(ICERs_vector)
+percentage_below_20000 <- sum(ICERs_vector<20000) / length
+percentage_below_80000 <- sum(ICERs_vector<80000) / length
+
+# This seems wrong. 
+# The south-east quadrant should have negative ICERs (you are saving money while gaining utility)
+# However the north-west quadrant should have large ICERs because the new treatment is costing you money 
+# while also resulting in harm.
+
+
+# Let's calculate the fraction for 200 WTPs: from 0 to 200,000€
+percentages_vector <- vector()
+for (i in seq(0, 200000, length.out = 201)) {
+  percentage <- sum(ICERs_vector<i) / length
+  percentages_vector <- c(percentages_vector, percentage)
+}
+
+plot(x=seq(0, 200000, length.out = 201), y=percentages_vector, xlab="Willingness to Pay Threshold",
+  ylab="Points below WTP curve", main="CEAC curve")
